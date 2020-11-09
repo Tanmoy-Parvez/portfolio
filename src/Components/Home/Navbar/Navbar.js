9e4845a5d20eeeef37dec923da11b2e4f5361ed3
israfil-cse <israfilhossen10750@gmailcom>
2020-11-09 16:56:00 +0600

    nav overview

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -7,16 +7,16 @@ const Navbar = () => {
     return (
 
         <div className=" navSectio">
-            <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+            <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3">
                <div  className="container">
-               <a style={{color:"#111430"}} class="navbar-brand  btn btn-light text-dark" href="https://pdfhost.io/v/bkHHX6NQ2_Israfil_HossenWeb_Developerresumepdf.pdf" target="-blank">Download Resume</a>
+               <a style={{color:"#111430"}} class="navbar-brand  btn btn-light text-dark" href="https://pdfhost.io/v/9Pn6dgAjl_IsrafilHossenWebDevresumepdf.pdf" target="-blank">Download Resume</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto ">
                         <li class="nav-item  ">
-                            <Link to="/Home" class="nav-link" >Home <span class="sr-only">(current)</span></Link>
+                            <Link to="/Home" class="nav-link " >Home <span class="sr-only">(current)</span></Link>
                         </li>
                         <li class="nav-item">
                             <Link to="/project" class="nav-link" >Project</Link>
